0ff05b1575cbccb23fc824f98f70d1844d11d7b8
redjax <no@none.com>
2025-03-09 15:00:40 -0400

    It's broken, I don't like making TUIs.

--- sandbox/tui/torrentmgr.py
+++ sandbox/tui/torrentmgr.py
@@ -1,20 +1,16 @@
 from textual.app import App, ComposeResult
 from textual.containers import VerticalScroll, Container
-from textual.widgets import Footer, Header, Button, DataTable
+from textual.widgets import Footer, Header, Button, DataTable, SelectionList
 from textual.reactive import reactive
 from textual import on
 import transmission_lib
 
 
-class TorrentsList(VerticalScroll):
+class TorrentsList(SelectionList):
     """Class to mount torrents in a vertical scrolling table, where each row is a torrent."""
 
     def compose(self) -> ComposeResult:
-        yield Container(
-            DataTable(id="torrent_table"),
-            Button("Pause Selected", id="pause_button"),
-            Button("Resume Selected", id="resume_button"),
-        )
+        yield Container(DataTable(id="torrent_table"))
 
 
 class TransmissionApp(App):
@@ -37,44 +33,6 @@ class TransmissionApp(App):
         self.controller = transmission_lib.get_transmission_controller(
             transmission_settings=self.transmission_settings
         )
-
-        table = self.query_one("#torrent_table", DataTable)
-        table.add_columns("ID", "Name", "Status", "Progress")
-        table.cursor_type = "row"  # This allows row selection
-
-        self.refresh_torrents()
-
-    def refresh_torrents(self) -> None:
-        self.torrents = self.controller.get_all_torrents()
-
-        table = self.query_one("#torrent_table", DataTable)
-        table.clear()
-
-        # table.add_columns("ID", "Name", "Status", "Progress")
-
-        for torrent in self.torrents:
-            table.add_row(
-                str(torrent.id),
-                torrent.name,
-                torrent.status,
-                f"{torrent.progress:.2f}%",
-            )
-
-    @on(Button.Pressed, "#pause_button")
-    def pause_selected(self) -> None:
-        table = self.query_one("#torrent_table", DataTable)
-        for row in table.selected_rows:
-            torrent_id = int(table.get_cell_at((row, 0)))
-            self.controller.stop_torrent_by_id(torrent_id)
-        self.refresh_torrents()
-
-    @on(Button.Pressed, "#resume_button")
-    def resume_selected(self) -> None:
-        table = self.query_one("#torrent_table", DataTable)
-        for row in table.selected_rows:
-            torrent_id = int(table.get_cell_at((row, 0)))
-            self.controller.start_torrent_by_id(torrent_id)
-        self.refresh_torrents()
 
 
 if __name__ == "__main__":
