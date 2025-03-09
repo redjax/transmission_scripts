f0b6b17b6800110c85c68ebb29219a39c3f7e2f5
redjax <no@none.com>
2025-03-09 14:04:08 -0400

    Add hello tui app

--- sandbox/tui/helloworld.py
+++ sandbox/tui/helloworld.py
@@ -3,6 +3,14 @@ from textual.widgets import Header, Footer, Static
 
 
 class HelloWorldApp(App):
+    CSS = """
+    #hello-text {
+        color: green;
+        background: black;
+        border: round yellow;
+    }
+    """
+
     def compose(self):
         yield Header()
         yield Static("Hello, World!", id="hello-text")
